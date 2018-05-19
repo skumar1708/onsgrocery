f684020015954d6154021f36487459ac22ef6556
Sharvan <itbhu.sk@gmail.com>
2018-05-19 08:16:07 +0530

    intial push prac

--- www/js/index.js
+++ www/js/index.js
@@ -1,28 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
     // Bind Event Listeners
-    //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
@@ -42,11 +23,6 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
-
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
         document.addEventListener("offline", app.onOffline, false);
@@ -86,7 +62,8 @@ function launchStore(url){
 		});
 		ref.addEventListener('loadstop', function(event) {
 			//iniDiv.style.display = "none";
-			ref.show();
+            ref.show();
+            setupPush();
 		});
 		ref.addEventListener('exit', function(event) {
 							swal({
@@ -108,3 +85,62 @@ function launchStore(url){
 										});
 		});
 }   
+
+function setupPush() {
+    alert('calling push init');
+    var push = PushNotification.init({
+        "android": {
+            "senderID": "20215523485"
+        },
+        "browser": {},
+        "ios": {
+            "sound": true,
+            "vibration": true,
+            "badge": true
+        },
+        "windows": {}
+    });
+    alert('after init');
+    push.on('registration', function(data) {
+        alert('registration event: ' + data.registrationId);
+
+        var oldRegId = localStorage.getItem('registrationId');
+        if (oldRegId !== data.registrationId) {
+            // Save new registration ID
+            localStorage.setItem('registrationId', data.registrationId);
+        
+            // unsubscribe and resubscribe
+            push.unsubscribe("all", function () {
+                alert("unsubscribed to all");
+                push.subscribe("allNew", function successSubscribe () {
+                    // success ...
+                    alert("resubscribed to all");
+                }, function errorSubscribe () {
+                    // error ...
+                    alert("error subscribing to all");
+                });
+            });
+        }
+
+        var parentElement = document.getElementById('registration');
+        var listeningElement = parentElement.querySelector('.waiting');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    });
+
+    push.on('error', function(e) {
+        alert("push error = " + e.message);
+    });
+
+    push.on('notification', function(data) {
+        alert('notification event');
+        navigator.notification.alert(
+            data.message,         // message
+            null,                 // callback
+            data.title,           // title
+            'Ok'                  // buttonName
+        );
+   });
+}
